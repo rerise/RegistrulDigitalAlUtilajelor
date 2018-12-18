3fd4bbbb3ee6662dfa76830d07324b4369b54544
Diana M <mdiannna@yahoo.com>
2018-12-18 11:44:27 +0200

    Added auth middleware to admin pages

--- routes/web.php
+++ routes/web.php
@@ -15,34 +15,38 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
-// TODO: register, login & prefixare cu /admin/??
-Route::resource('machines', 'MachinesController');
-Route::resource('organizations', 'OrganizationsController');
-Route::resource('companies', 'CompaniesController');
-Route::resource('deposits', 'DepositsController');
-Route::resource('operators', 'OperatorsController');
-
-Route::resource('machine-parameters', 'MachineParametersController');
-Route::resource('machine-categories', 'MachineCategoriesController');
-
-// Route::get('/machines', ['uses' => 'MachinesController@list', 'as' => 'machines.list']);
-
-
-// Route::get('/machines/create', ['uses' => 'MachinesController@create', 'as' => 'machines.create']);
-
-// Route::get('/machines/store', ['uses' => 'MachinesController@store', 'as' => 'machines.store']);
-
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-
-Route::get('/template', function() {
-	return view('template');
-});
+// Route::get('/home', 'HomeController@index')->name('home');
 
 
-Route::get('/about', function() {
-	return view('about');
+
+Route::group(['middleware' => ['auth']], function () {
+   
+	Route::get('/template', function() {
+		return view('template');
+	});
+
+
+	// TODO: register, login & prefixare cu /admin/??
+	Route::resource('machines', 'MachinesController');
+	Route::resource('organizations', 'OrganizationsController');
+	Route::resource('companies', 'CompaniesController');
+	Route::resource('deposits', 'DepositsController');
+	Route::resource('operators', 'OperatorsController');
+
+	Route::resource('machine-parameters', 'MachineParametersController');
+	Route::resource('machine-categories', 'MachineCategoriesController');
+
+	// Route::get('/machines', ['uses' => 'MachinesController@list', 'as' => 'machines.list']);
+
+
+	// Route::get('/machines/create', ['uses' => 'MachinesController@create', 'as' => 'machines.create']);
+
+	// Route::get('/machines/store', ['uses' => 'MachinesController@store', 'as' => 'machines.store']);
+
+	 //
+	
+	Route::get('/about', function() {
+		return view('about');
+	});
 });
