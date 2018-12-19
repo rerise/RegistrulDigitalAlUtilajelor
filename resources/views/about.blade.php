d70c98e9c93c6b7f378d6c6057fa4f2b6b96d02a
Diana M <mdiannna@yahoo.com>
2018-12-20 00:12:20 +0200

    Added description

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -24,7 +24,14 @@
 @section('content')
 <div>               
    
-   <p><b>Registrul utilajelor</b> este un proiect iniţiat de către Re:Rise (Asociaţia pentru Reducerea Riscului Seismic cu scopul de a pune la dispoziţia DSU ...........</p>
+   <p><b>Registrul (Digital al) Utilajelor</b> este un proiect inițiat de către Re:Rise (Asociația pentru Reducerea Riscului Seismic) care pune la dispoziția DSU (Departamentul pentru Situații de Urgență - principala autoritate responsabilă pentru prevenirea și gestionarea situațiilor de urgență la nivel național) o bază de date cu utilajele deținute de către operatori privați (buldozere, excavatoare etc.), care să ajute la răspunsul post-seism al forțelor de salvare.
+<br>
+<br>
+Prin intermediul Registrului (Digital al) Utilajelor, DSU va putea avea acces instant, în timp real, la informații legate de locația și disponibilitatea utilajelor private, iar autoritățile le vor putea prelua de urgență pentru a fi folosite la intervențiile necesare după un cutremur major: eliberarea drumurilor și a căilor de acces, salvarea persoanelor de sub dărâmături, punerea în siguranță a structurilor afectate ș.a.
+<br>
+<br>
+
+Conform legilor în vigoare din România, în momentul declarării stării de necesitate, autoritățile responsabile pentru intervenție pot prelua, pe o durată limitată, utilaje private necesare acțiunilor de salvare. Registrul (Digital al) Utilajelor vine în sprijinul DSU pentru a accelera procesul de salvare și pentru a limita numărul de victime.</p>
 </div>
        
 @endsection
